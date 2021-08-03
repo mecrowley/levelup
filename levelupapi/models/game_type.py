a4ec013447a1d364b35ecede202e8a9533bd572e
Madeline Crowley <madelinecrowley@gmail.com>
2021-08-03 11:02:16 -0500

    ch 4 - added fixtures

---
+++ levelupapi/models/game_type.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class GameType(models.Model):
+    """GameType model
+    fields:
+        label (CharField): name of the game type
+    """
+    label = models.CharField(max_length=50)
